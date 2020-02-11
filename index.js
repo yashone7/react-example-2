30e2a2a34931067ebd0fcc7fb2aaf4d8e90d26e8
Yashwanth <yashwanth@pop-os.localdomain>
2020-02-11 18:54:52 +0530

    added a comment in index file

--- index.js
+++ index.js
@@ -3,4 +3,5 @@ import ReactDOM from "react-dom";
 import App from "./Application/App";
 import "./node_modules/bootstrap/dist/css/bootstrap.min.css";
 
+// Here we take the app component and mount it in root
 ReactDOM.render(<App />, document.getElementById("root"));
